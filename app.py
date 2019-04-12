7130ef502806f0fbea1c3071d4d3ad902dcbec28
Elizabeth Keleshian <ekeleshian@gmail.com>
2019-04-12 04:54:13 -0700

    edit code to match function signature

--- app.py
+++ app.py
@@ -10,24 +10,29 @@ df = pd.read_csv('emoji_dict.csv')
 
 @hug.get('/emojitype/{input}')
 def get_parser(input: str):
+	"""
+	Returns a dictionary of found emojis (if any) with their corresponding labels, 
+	given a string as input. Returns an empty dictionary if no emoji is found in string.
+	"""
 	cats = ['person', 'thing', 'place', 'time', 'activity', 'mood']
-	def extract_emojis(string):
+	def emoji_to_types(string):
 		"""
 		Tool to extract emojis from string of text
 
 		Input:  string with some (if any) emojis
-		Ouput:  <list> list of the emojis. If no emojis found, returns empty list.
+		Ouput:  dictionary containing discovered emojies and their corresponding label.
 		"""
 		emos = emoji.emoji_lis(string)
-		return emos
 
-	emojis = extract_emojis(input)
-	result = dict()
-	for idx, emo in enumerate(emojis):
-		descr = emoji.UNICODE_EMOJI_ALIAS[emo['emoji']]
-		cat = df[df.EMOJI_ALIAS_UNICODE==descr].VEC_TO_CAT.item()
-		result[cat] = result.get(cat, []) + [emo['emoji']]
+		result = dict()
+		for idx, emo in enumerate(emos):
+			descr = emoji.UNICODE_EMOJI_ALIAS[emo['emoji']]
+			cat = df[df.EMOJI_ALIAS_UNICODE==descr].VEC_TO_CAT.item()
+			result[cat] = result.get(cat, []) + [emo['emoji']]
+		return result
 
-	return result
+	emoji_types = emoji_to_types(input)
+
+	return emoji_types
 
 
